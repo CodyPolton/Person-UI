4acee0be8ea57203774f48e523072df57e8814c9
CodyPolton <43789835+CodyPolton@users.noreply.github.com>
2019-09-11 20:05:29 -0500

    Uploaded all the files

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Cody Polton on 10/09/2019.
+//  Copyright © 2019 Cody Polton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
